3ff0505b8a6983c8c6914af6b5b323d9393ecf65
ajrotert <andrew.rotert@trojans.dsu.edu>
2020-02-11 12:58:10 -0600

    ...

--- Scripts/Connection.js
+++ Scripts/Connection.js
@@ -1,14 +1,14 @@
 ﻿            // Your web app's Firebase configuration
 
-    var firebaseConfig = {
-    apiKey: "AIzaSyAm7Mmd1nnzDxnsiG5mmMDvF1ACz3Pp3UA",
-    authDomain: "connectwebpagetofirebase.firebaseapp.com",
-    databaseURL: "https://connectwebpagetofirebase.firebaseio.com",
-    projectId: "connectwebpagetofirebase",
-    storageBucket: "connectwebpagetofirebase.appspot.com",
-    messagingSenderId: "169507978975",
-    appId: "1:169507978975:web:dd12cf0390b8188c0a336a",
-    measurementId: "G-VM4FQ6EBQV"
+var firebaseConfig = {
+apiKey: "AIzaSyAm7Mmd1nnzDxnsiG5mmMDvF1ACz3Pp3UA",
+authDomain: "connectwebpagetofirebase.firebaseapp.com",
+databaseURL: "https://connectwebpagetofirebase.firebaseio.com",
+projectId: "connectwebpagetofirebase",
+storageBucket: "connectwebpagetofirebase.appspot.com",
+messagingSenderId: "169507978975",
+appId: "1:169507978975:web:dd12cf0390b8188c0a336a",
+measurementId: "G-VM4FQ6EBQV"
 };
 // Initialize Firebase
 
