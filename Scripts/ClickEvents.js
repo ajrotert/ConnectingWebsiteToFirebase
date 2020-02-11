5a7c0508361d87cd2202305986962393e186b2e7
ajrotert <andrew.rotert@trojans.dsu.edu>
2020-02-11 11:35:44 -0600

    Minor changes

---
+++ Scripts/ClickEvents.js
@@ -0,0 +1,29 @@
+﻿document.getElementById("test-button").onclick = function () {
+    firebase.database().ref('users/' + "{First_Name Last_Name}").set({
+        email: "Email",
+        username: "Username",
+        password: "Password"
+    });
+
+}
+
+document.getElementById("delete-button").onclick = function () {
+    firebase.database().ref().remove();
+}
+
+document.getElementById("user-button").onclick = function () {
+    var fn = document.getElementsByName("firstname").values;
+    var ln = document.getElementsByName("lastname").values;
+    var em = document.getElementsByName("email").values;
+    var user = document.getElementsByName("username").values;
+    var pass = document.getElementsByName("password").values;
+
+    //broken
+
+    firebase.database().ref('users/' + fn+"_"+ln).set({
+        email: em,
+        username: user,
+        password: pass
+    });
+
+}
---
+++ Scripts/Connection.js
@@ -0,0 +1,35 @@
+﻿            // Your web app's Firebase configuration
+
+    var firebaseConfig = {
+    apiKey: "AIzaSyAm7Mmd1nnzDxnsiG5mmMDvF1ACz3Pp3UA",
+    authDomain: "connectwebpagetofirebase.firebaseapp.com",
+    databaseURL: "https://connectwebpagetofirebase.firebaseio.com",
+    projectId: "connectwebpagetofirebase",
+    storageBucket: "connectwebpagetofirebase.appspot.com",
+    messagingSenderId: "169507978975",
+    appId: "1:169507978975:web:dd12cf0390b8188c0a336a",
+    measurementId: "G-VM4FQ6EBQV"
+};
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+//Data Object Change Listener
+
+const preObject = document.getElementById('object');
+
+const dbRefObject = firebase.database().ref();
+
+
+dbRefObject.on('value', snap => {
+
+    console.log(snap.val());
+    preObject.innerText = JSON.stringify(snap.val(), null, 3);
+
+}, function (error) {
+    // The fetch failed.
+
+    console.error(error);
+});
