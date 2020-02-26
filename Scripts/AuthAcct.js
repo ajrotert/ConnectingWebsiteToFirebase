03e452c95e6e1376816ec1f2e90f91881393ef00
ajrotert <andrew.rotert@trojans.dsu.edu>
2020-02-26 15:30:32 -0600

    Using firebase auth

--- Scripts/AuthAcct.js
+++ Scripts/AuthAcct.js
@@ -1,38 +1,18 @@
-﻿document.getElementById("start-button").onclick = function () {
+﻿
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-    var user = document.getElementById("username").value;
-    var pass = document.getElementById("password").value;
+document.getElementById("start-button").onclick = function () {
 
-    //May be used for some kind of authentication later on
     var passed = false;
 
-    if (user != "" && pass != "") {
-        user_info.username = user;
-        user_info.password = pass;
-        sessionStorage.setItem("USER-NAME", user_info.username);
-        sessionStorage.setItem("USER-PASS", user_info.password);
+    var promise = ui.start('#firebaseui-auth-container', {
+        signInSuccessUrl: './main.html',
+        signInOptions: [
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ]
 
-        passed = true;
-        const promise = firebase.database().ref('Users/' + user_info.username).set({
-            username: user_info.username,
-            password: user_info.password
-        });
-        promise.then(() => {
-            setTimeout(1000);
-            window.location.href = "./main.html";
-        });
+    });
 
-    }
-    if (!passed)
-        alert("Please check the username and password fields for valid information.")
-
-    document.getElementById("user-form").reset();
 
 }
 
-var today = new Date();
-
-var user_info = {
-    username: "Guest User",
-    password: today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
-};
